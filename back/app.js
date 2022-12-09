29262bcc1c654b36eb8fa598bfb3ddb238a80c22
Antoine Labard <antoine@labard.fr>
2022-12-09 09:57:00 +0100

    feat: ➕ add MongoDB integration

--- back/app.js
+++ back/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://antoinelabard:${process.env.MONGO_PASSWORD}@cluster0.bh7ggjl.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connection to Mongoose successful'))
+  .catch(() => console.log('Connection to Mongoose failed'));
+
 app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
+    res.json({ message: 'Votre requête a bien été reçue !' });
  });
 
 module.exports = app;
