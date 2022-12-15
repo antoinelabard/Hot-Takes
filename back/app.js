3a9075dc1f75eafb404b3321839966a5eeab6c12
Antoine Labard <antoine@labard.fr>
2022-12-15 11:39:08 +0100

    fix: 🐛 app.js

--- back/app.js
+++ back/app.js
@@ -3,16 +3,40 @@ require('dotenv').config()
 const app = express();
 let authRoutes = require('./routes/auth')
 let sauceRoutes = require('./routes/sauce')
+let helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+let mongoSanitize = require('express-mongo-sanitize')
+const path = require('path')
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose.connect(`mongodb+srv://antoinelabard:${process.env.MONGO_PASSWORD}@cluster0.bh7ggjl.mongodb.net/?retryWrites=true&w=majority`,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connection to Mongoose successful'))
-  .catch(() => console.log('Connection to Mongoose failed'));
- 
-app.use('/api/auth', authRoutes)
-app.use('/api/sauces', sauceRoutes)
+  .catch(() => console.log('Connection to Mongoose failed'))
 
-module.exports = app;
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Vous avez atteint le nombre de requête maximum, merci de réessayer dans un instant."
+})
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+app.use(express.json())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use('/api/auth', apiLimiter, authRoutes)
+app.use('/api/sauces', sauceRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+module.exports = app
